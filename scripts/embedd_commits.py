daf52a211faff982a3cc4dee23ed9bab1143de3c
7db9a <noreply@yourdomain.com>
2024-09-02 19:18:36 -0400

    Update variable names in embedd_commits.py for clarity
    - Change `commit_logs_file_path` to `commits_logs_file_path`
    - Update `commit_logs_json` to `commits_logs_json`
    - Ensure consistency with other variable names and comments
    - Enhance readability and maintainability by using plural form

--- scripts/embedd_commits.py
+++ scripts/embedd_commits.py
@@ -5,20 +5,20 @@ from lib.utils.utilities import read_json_file, write_json_file
 
 logging.basicConfig(level=logging.INFO)
 
-commit_logs_file_path = 'data/commit_logs.json'  # Path to your JSON file
-commit_logs_json = read_json_file(commit_logs_file_path)
+commits_logs_file_path = 'data/commits_logs.json'  # Path to your JSON file
+commits_logs_json = read_json_file(commits_logs_file_path)
 
 commits_embeddings_file_path = 'data/commits_embeddings.json'  # Path to your JSON file
 existing_commits_embeddings_json = read_json_file(commits_embeddings_file_path)
 
-generator = CommitEmbeddingGenerator(commit_logs_json, existing_commits_embeddings_json)
+generator = CommitEmbeddingGenerator(commits_logs_json, existing_commits_embeddings_json)
 updated_commits_embeddings_json = generator.generate_embeddings()
 
 # Write updated logs back to the JSON file
 write_json_file(updated_commits_embeddings_json, commits_embeddings_file_path)
 
 # Example usage:
-# commit_logs = [...]  # List of commit dictionaries
+# commits_logs = [...]  # List of commit dictionaries
 # existing_embeddings = {...}  # JSON object with existing embeddings
-# generator = CommitEmbeddingGenerator(commit_logs, existing_embeddings)
+# generator = CommitEmbeddingGenerator(commits_logs, existing_embeddings)
 # updated_embeddings = generator.generate_embeddings()
